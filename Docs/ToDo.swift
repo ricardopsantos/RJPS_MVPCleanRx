04cfbd0d72ff5b5aea17303ced458d413f60663f
Ricardo Santos <ricardo.psantos@innowave.tech>
2019-07-30 08:08:56 +0100

    swiftlint

--- Docs/ToDo.swift
+++ Docs/ToDo.swift
@@ -1,10 +1,5 @@
-
 #warning("add plist config file")
-
 #warning("improve alerts top message (have messages colision)")
-
 #warning("add debug label/screen/html report")
-
 #warning("adicionar image a mostrar os ecran os tipo de botoes e labels")
-
 #warning ("adicionar cache")
