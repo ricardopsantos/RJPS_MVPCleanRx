41427d23d6958c6fd47ccb6fffe7810781824f09
Ricardo Santos <rsantos@blissapplications.com>
2020-05-09 21:03:28 +0100

    AppCore

---
+++ AppCore/AppCore.h
@@ -0,0 +1,19 @@
+//
+//  AppCore.h
+//  AppCore
+//
+//  Created by Ricardo Santos on 09/05/2020.
+//  Copyright © 2020 Ricardo P Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppCore.
+FOUNDATION_EXPORT double AppCoreVersionNumber;
+
+//! Project version string for AppCore.
+FOUNDATION_EXPORT const unsigned char AppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCore/PublicHeader.h>
+
+
