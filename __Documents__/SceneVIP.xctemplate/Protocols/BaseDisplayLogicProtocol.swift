ba1fabf890033aff4a727f6c38d81ec06aa4ee90
Ricardo Santos <rsantos@blissapplications.com>
2020-05-10 19:09:07 +0100

    Adding VIP Template : cleaning code

--- __Documents__/SceneVIP.xctemplate/Protocols/BaseDisplayLogicProtocol.swift
+++ __Documents__/SceneVIP.xctemplate/Protocols/BaseDisplayLogicProtocol.swift
@@ -20,38 +20,12 @@ public protocol InteratorMandatoryBusinessLogicProtocol {
     func requestScreenInitialState()
 }
 
-open class BaseInteractorWithWorkers: BaseInteractor {
-    // All workers
-    //let workerDashboard: DashboardWorker = DashboardWorker()
-}
-
 public protocol BasePresentationLogicProtocol: class {
     func presentLoading(response: LoadingModel)
     func presentError(response: ErrorModel)
 }
 
-public protocol BaseBusinessLogicProtocol {
-
-}
-
 open class BaseInteractor {
-    public var errorType: AppCodes?
     public var disposeBag = DisposeBag()
-
-    public init() {}
-
-    open func basePresentationLogicImpl() -> BasePresentationLogicProtocol? {
-        assert(false)
-        return nil
-    }
-
-    open func presentError(error: Error) {
-        let response = ErrorModel(error: error)
-        basePresentationLogicImpl()?.presentError(response: response)
-    }
-
-    open func presentLoading(isLoading: Bool, message: String = "") {
-        let response = LoadingModel(isLoading: isLoading, message: message)
-        basePresentationLogicImpl()?.presentLoading(response: response)
-    }
+    public init() { }
 }
